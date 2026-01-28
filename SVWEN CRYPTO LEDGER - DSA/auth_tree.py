becb9bbe1283098fa0d5d16cbb613115a0c653b2
Ahsan Tq Imran <iahsantq15@gmail.com>
2026-01-28 20:34:19 +0500

    Delete SVWEN CRYPTO LEDGER - DSA directory

--- SVWEN CRYPTO LEDGER - DSA/auth_tree.py
+++
@@ -1,5 +0,0 @@
-def authorize(is_authenticated):
-    if is_authenticated:
-        return True
-    else:
-        return False
--- SVWEN CRYPTO LEDGER - DSA/hash_util.py
+++
@@ -1,3 +0,0 @@
-def calculate_hash(index, timestamp, data, previous_hash):
-    hash_string = str(index) + timestamp + str(data) + previous_hash
-    return str(hash(hash_string))
